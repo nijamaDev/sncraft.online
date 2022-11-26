a8c8f76e6a20104caa7f96dec4987193fcc53d65
nijamaDev <nijama.dev@gmail.com>
2022-11-26 16:06:30 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'b30ef4d59c044d61be6d95f763aea959',
+  rootNotionPageId: '5949ed11bc46434a95fa612339ea5a37',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
