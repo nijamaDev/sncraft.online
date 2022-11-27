9d79b3cc58667729ed5dd434797a87cf0de89340
nijamaDev <nijama.dev@gmail.com>
2022-11-27 09:55:35 -0500

    sncraft

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '5949ed11bc46434a95fa612339ea5a37',
+  rootNotionPageId: 'b30ef4d59c044d61be6d95f763aea959',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
